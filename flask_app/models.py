2b0a373c4fd8d8218147ebdb88a0a46762c6ef7e
RichardM83 <rjmartinez422@gmail.com>
2024-04-14 16:31:02 -0400

    Functionality updates (mostly copied from fall23->inclass->week7)

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,3 +1,14 @@
 from flask_login import UserMixin
 from datetime import datetime
 from . import db, login_manager
+
+
+class User(db.Document, UserMixin):
+    email = db.EmailField(unique=True, required=True)
+    username = db.StringField(unique=True, required=True)
+    password = db.StringField(required=True)
+    profile_pic = db.ImageField()
+    #TODO: I think we need a highscore option here
+
+    def get_id(self):
+        return self.username
