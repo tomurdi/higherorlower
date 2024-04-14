52b08c9d8aa06013e4a15bf22a408003063be88d
RichardM83 <rjmartinez422@gmail.com>
2024-04-14 14:25:23 -0400

    setup

---
+++ flask_app/__init__.py
@@ -0,0 +1,16 @@
+# 3rd-party packages
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mongoengine import MongoEngine
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_user,
+    logout_user,
+    login_required,
+)
+from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
+
+db = MongoEngine()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
---
+++ flask_app/models.py
@@ -0,0 +1,3 @@
+from flask_login import UserMixin
+from datetime import datetime
+from . import db, login_manager
