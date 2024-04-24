d875e0f1da08991c484d1a5ec06395e8c412b3b0
tomurdi <root@urdinola.localdomain>
2024-04-24 18:07:52 -0400

    api config

--- flask_app/client.py
+++ flask_app/client.py
@@ -4,17 +4,29 @@
 
 import requests
 
-class Game(object):
-    def __init__(self, omdb_json, detailed=False) -> None:
-        if detailed:
-            self.name = omdb_json["name"]
-            self.rating = omdb_json["rating"]
-            self.image = omdb_json["background image"]
-            self.downloads = omdb_json["added"]
+# class Game(object):
+#     def __init__(self, omdb_json, detailed=False) -> None:
+#         if detailed:
+#             self.name = omdb_json["results"]["name"]
+#             self.rating = omdb_json["results"]["rating"]
+#             self.image = omdb_json["results"]["background_image"]
+#             self.downloads = omdb_json["results"]["added"]
 
 class GameClient(object):
-    def __init__(self,api_key) -> None:
+    def __init__(self,) -> None:
         self.sess = requests.Session()
-        self.base_url = f"https://api.rawg.io/api/games?key={api_key}"
+        self.base_url = f"https://api.rawg.io/api/games?key=ae19bf1807254a92af216b11d467792b"
 
-        
+    def get_game_list(self):
+        games = []
+        resp = self.sess.get(f'{self.base_url}')
+        # return resp.json()['results']
+        for game_dict in resp.json()['results']:
+            tup = (game_dict["name"],game_dict["rating"],game_dict["added"],game_dict["background_image"])
+            games.append(tup)
+        return games
+
+if __name__ == '__main__':
+    client = GameClient()
+    l = client.get_game_list()
+    print(l)
