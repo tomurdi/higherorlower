202c0521286d3b42f4c0bb1b853bf00cdf624f49
RichardM83 <rjmartinez422@gmail.com>
2024-04-25 13:50:39 -0400

    Udjusted game route to include game_list

--- flask_app/game/routes.py
+++ flask_app/game/routes.py
@@ -3,12 +3,15 @@ import base64, io
 from io import BytesIO
 from flask_login import current_user
 from ..models import User
+from ..client import GameClient
 
 game = Blueprint('game', __name__)
+game_client = GameClient()
 
 @game.route('/play')
 def play():
-    return render_template('game.html')
+    return render_template('game.html', 
+                           game_list=sorted(game_client.get_game_list(),key=lambda x:x[3]))
 
 @game.route('/leaderboard')
 def leaderboard():
