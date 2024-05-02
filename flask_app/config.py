644e03a31733cf8a85d70e11a9384e172f8dfb02
tomurdi <root@urdinola.localdomain>
2024-05-02 18:11:05 -0400

    imported os

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,6 @@
 # Stores all configuration values
 import os
-SECRET_KEY = b'\xc6M9\xe9\xf7<r+\x87;8\xd1\xbd\xcb\x91\x9c'
-MONGODB_HOST = 'mongodb+srv://Mittens1415:nRl%38jJUokLjxVDoa@cluster1415.r81rd4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1415'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# MONGODB_HOST = os.environ.get('MONGODB_HOST')
+# SECRET_KEY = b'\xc6M9\xe9\xf7<r+\x87;8\xd1\xbd\xcb\x91\x9c'
+# MONGODB_HOST = 'mongodb+srv://Mittens1415:nRl%38jJUokLjxVDoa@cluster1415.r81rd4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1415'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGODB_HOST = os.environ.get('MONGODB_HOST')
